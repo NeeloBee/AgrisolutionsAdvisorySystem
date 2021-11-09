6b3ab641be657e171e1f6c042abe64c4c3bc358c
Omphile <201130339@student.uj.ac.za>
2021-11-09 22:31:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let app = express();
+
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/', function(req, res){
+    res.render('index');
+
+});
+let PORT = process.env.PORT || 3007;
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
+
+
