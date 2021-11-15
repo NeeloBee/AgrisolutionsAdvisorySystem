918da7a5a4a9d09ed893ef238a0da2e05e1c90a4
Omphile <201130339@student.uj.ac.za>
2021-11-15 22:31:54 +0200

    updated the node version and renamed the files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let express = require('express');
-let app = express();
+
 
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -8,62 +8,75 @@ const bodyParser = require('body-parser');
 // import sqlite modules
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
+let app = express();
 
-
-let PORT = process.env.PORT || 3007;
-
-app.listen(PORT, function(){
-  console.log('App starting on port', PORT);
-});
-
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: false}));
 app.set('view engine', 'handlebars');
 
+
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+// parse application/json
+app.use(express.json())
 app.use(express.static('public'));
 
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
-// // parse application/json
-// app.use(bodyParser.json())
-
-
-// open({
-// 	filename: './login.db',
-// 	driver: sqlite3.Database
-// }).then(async function (db) {
-
-// 	// run migrations
-
-// 	await db.migrate();
-
-// 	// only setup the routes once the database connection has been established
-//   app.get('/', function(req, res){
-//     db
-//     .all('select * from login')
-//     .then (function(login){
-//       console.log(login);
-
-//       res.render('login', {
-//         login
-//       });
-//     })
-    
-
-// });
-
-// })
 
 
 
 
-app.get('/', function (req, res) {
-  // console.log(settingsBill.returnColor());
-    res.render('home')
 
 
-    
-});
+open({
+	filename: './login.db',
+	driver: sqlite3.Database
+}).then(async function (db) {
+
+	// run migrations
+
+	await db.migrate();
+
+	// only setup the routes once the database connection has been established
+  // app.get('/', function(req, res){
+  //   db
+  //   .all('select * from login')
+  //   .then (function(login){
+  //     console.log(login);
+
+  //     res.render('login', {
+  //       login
+  //     });
+  //   })
+
+  // }); 
+
+
+  app.get('/', function (req, res) {
+    res.render('login') 
+  });
+  
+  app.get('/login', function (req, res) {
+    res.render('login') 
+  });
+  
+  app.get('/home', function (req, res) {
+    res.render('home') 
+  });
+  
+  
+  let PORT = process.env.PORT || 3007;
+  
+  app.listen(PORT, function(){
+    console.log('App starting on port', PORT);
+  });
+})
+
+
+
+
+
 
 
 
