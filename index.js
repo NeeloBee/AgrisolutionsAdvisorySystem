f091a05fdf76570f5a0f0ef99ce15f74057b41df
Omphile <201130339@student.uj.ac.za>
2021-11-15 15:00:46 +0200

    updated the node version on Json File

--- index.js
+++ index.js
@@ -10,47 +10,61 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
 
+let PORT = process.env.PORT || 3007;
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/json
+// app.use(bodyParser.json())
 
 
-open({
-	filename: './login.db',
-	driver: sqlite3.Database
-}).then(async function (db) {
+// open({
+// 	filename: './login.db',
+// 	driver: sqlite3.Database
+// }).then(async function (db) {
 
-	// run migrations
+// 	// run migrations
 
-	await db.migrate();
+// 	await db.migrate();
 
-	// only setup the routes once the database connection has been established
-  app.get('/', function(req, res){
-    db
-    .all('select * from login')
-    .then (function(login){
-      console.log(login)
-    })
-    res.render('index');
+// 	// only setup the routes once the database connection has been established
+//   app.get('/', function(req, res){
+//     db
+//     .all('select * from login')
+//     .then (function(login){
+//       console.log(login);
 
+//       res.render('login', {
+//         login
+//       });
+//     })
+    
+
+// });
+
+// })
+
+
+
+
+app.get('/', function (req, res) {
+  // console.log(settingsBill.returnColor());
+    res.render('home')
+
+
+    
 });
 
-})
 
-
-
-
-let PORT = process.env.PORT || 3007;
-
-app.listen(PORT, function(){
-  console.log('App starting on port', PORT);
-});
 
 
